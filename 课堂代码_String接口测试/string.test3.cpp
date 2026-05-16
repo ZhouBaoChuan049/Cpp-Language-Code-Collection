65e5a2721324341bf8baf0677d79967614dc2cf0
ZhouHan20 <3450014365@qq.com>
2026-05-16 19:34:38 +0800

    课堂代码_String接口测试

---
+++ 课堂代码_String接口测试/string.test3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//int main()
+//{
+//	string str0("a");
+//	int n = 999;
+//	int old_capacity = 0;
+//	for (int i = 0; i < n; i++)
+//	{
+//		str0.push_back('b');
+//		if (old_capacity != str0.capacity())
+//		{
+//			cout << "capacity: " << str0.capacity() << endl;
+//		}
+//		old_capacity=str0.capacity();
+//	}
+//	return 0;
+//}
---
+++ 课堂代码_String接口测试/string_test_2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+//int main()
+//{
+//	string str("yuanshenniubi");
+//	string::iterator it = str.begin();
+//	while (it != str.end())
+//	{
+//		*it += 1;
+//		cout << *it << " ";
+//		it++;
+//	}
+//	cout << endl;
+//	for (auto& ch : str)
+//	{
+//		ch--;
+//		cout << ch << " ";
+//	}
+//	cout << endl;
+//	string::iterator it1 = str.begin();
+//	while (it1 != str.end())
+//	{
+//		cout << *it1 << " ";
+//		it1++;
+//	}
+//	return 0;
+//}
