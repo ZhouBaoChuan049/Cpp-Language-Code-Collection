6c067758f9c1cee49eb42daadc585c5b0e639872
ZhouHan20 <3450014365@qq.com>
2026-06-03 10:21:13 +0800

    添加了引用折叠与完美转发的List以及相关的测试代码

---
+++ blog_cpp_12/test.cpp
@@ -0,0 +1,183 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<vector>
+#include<iostream>
+#include<map>
+#include<string>
+#include<assert.h>
+using namespace std;
+
+struct Point
+{
+	int _x;
+	int _y;
+};
+
+namespace bit
+{
+	class string
+	{
+	public:
+		typedef char* iterator;
+		typedef const char* const_iterator;
+
+		iterator begin()
+		{
+			return _str;
+		}
+
+		iterator end()
+		{
+			return _str + _size;
+		}
+
+		const_iterator begin() const
+		{
+			return _str;
+		}
+
+		const_iterator end() const
+		{
+			return _str + _size;
+		}
+
+		string(const char* str = "")
+			:_size(strlen(str))
+			, _capacity(_size)
+		{
+			cout << "string(char* str)-gouzao" << endl;
+			_str = new char[_capacity + 1];
+			strcpy(_str, str);
+		}
+
+		// 拷贝构造
+		string(const string& s)
+			:_str(nullptr)
+		{
+			cout << "string(const string& s) -- kbgouzao" << endl;
+			reserve(s._capacity);
+			for (auto ch : s)
+			{
+				push_back(ch);
+			}
+		}
+
+		void swap(string& ss)
+		{
+			::swap(_str, ss._str);
+			::swap(_size, ss._size);
+			::swap(_capacity, ss._capacity);
+		}
+
+		// 移动构造
+		string(string&& s)
+		{
+			cout << "string(string&& s) -- yidonggouzao" << endl;
+			// 转移掠夺你的资源
+			swap(s);
+		}
+
+		string& operator=(const string& s)
+		{
+			cout << "string& operator=(const string& s) -- kaobeifuzhi" <<
+				endl;
+			if (this != &s)
+			{
+				_str[0] = '\0';
+				_size = 0;
+				reserve(s._capacity);
+				for (auto ch : s)
+				{
+					push_back(ch);
+				}
+			}
+			return *this;
+		}
+
+		// 移动赋值
+		string& operator=(string&& s)
+		{
+			cout << "string& operator=(string&& s) -- yidongfuzhi" << endl;
+			swap(s);
+			return *this;
+		}
+
+		~string()
+		{
+			//cout << "~string() -- 析构" << endl;
+			delete[] _str;
+			_str = nullptr;
+		}
+
+		char& operator[](size_t pos)
+		{
+			assert(pos < _size);
+			return _str[pos];
+		}
+
+		void reserve(size_t n)
+		{
+			if (n > _capacity)
+			{
+				char* tmp = new char[n + 1];
+				if (_str)
+				{
+					strcpy(tmp, _str);
+					delete[] _str;
+				}
+				_str = tmp;
+				_capacity = n;
+			}
+		}
+
+		void push_back(char ch)
+		{
+			if (_size >= _capacity)
+			{
+				size_t newcapacity = _capacity == 0 ? 4 : _capacity *
+					2;
+				reserve(newcapacity);
+			}
+			_str[_size] = ch;
+			++_size;
+			_str[_size] = '\0';
+		}
+
+		string& operator+=(char ch)
+		{
+			push_back(ch);
+			return *this;
+		}
+
+		const char* c_str() const
+		{
+			return _str;
+		}
+
+		size_t size() const
+		{
+			return _size;
+		}
+	private:
+		char* _str = nullptr;
+		size_t _size = 0;
+		size_t _capacity = 0;
+	};
+}
+
+#include<list>
+#include"mylist.h"
+
+int main()
+{
+	bit::list<bit::string> lt;
+	bit::string s1("11111111111");
+	lt.push_back(s1);
+
+	bit::string s2("33333333333");
+	lt.push_back(move(s2));
+
+	lt.push_back("22222222222");
+
+	return 0;
+}
